7a2df647c9b737591ccd7123297cafa50ccba153
Elouan <louloulfx1@hotmail.fr>
2020-01-28 19:05:43 +0100

    MineBlock function

--- main.js
+++ main.js
@@ -55,36 +55,46 @@ toRead.then(data => {
     let stmt = db.prepare(
       "INSERT INTO blocks (previousHash, hash, nbPageStart, nbPageFinish, firstRegistration, secondRegistration, thirdRegistration, fourthRegistration, fifthRegistration) VALUES (?,?,?,?,?,?,?,?,?)"
     );
-    let previousHash = 0;
-    for (let i = 0; i < data.toRead.pageArray.length; i += 5) {
-      iterationLeft = data.toRead.pageArray.length - i;
 
-      let encript = data.toRead.pageArray[i].nbPage;
+    function mineBlock(nbPageStart) {
+      let nowBlock = [];
+      let previousHash = 0;
+      nbPageStart = nbPageStart - 1;
+      // let T = 30 * Math.random(0, 1)
+      let nbPageFin = nbPageStart + 4;
+      if (nbPageFin > 707) {
+        nbPageFin = 708;
+        nbPageFin = nbPageFin - nbPageStart;
+        console.log(nbPageFin, "yes");
+      }
+      console.log(nbPageFin);
+      let encript = data.toRead.pageArray[nbPageStart].nbPage;
       let hash = crypto
         .createHmac("sha256", "bVwDy-R4CyM-VdPnW-pV2Fc-88F9M")
         .update(encript.toString())
         .digest("hex");
-      stmt.run(
+      nowBlock.push(
         previousHash,
         hash,
-        data.toRead.pageArray[i].nbPage,
-        iterationLeft > 5
-          ? data.toRead.pageArray[i + 4].nbPage
-          : data.toRead.pageArray.length,
-        data.toRead.pageArray[i].content,
-        iterationLeft > 1 ? data.toRead.pageArray[i + 1].content : "",
-        iterationLeft > 2 ? data.toRead.pageArray[i + 2].content : "",
-        iterationLeft > 3 ? data.toRead.pageArray[i + 3].content : "",
-        iterationLeft > 4 ? data.toRead.pageArray[i + 4].content : ""
+        nbPageStart,
+        nbPageFin,
+        data.toRead.pageArray[nbPageStart].content,
+        nbPageFin > 1 ? data.toRead.pageArray[nbPageStart + 1].content : "",
+        nbPageFin > 2 ? data.toRead.pageArray[nbPageStart + 2].content : "",
+        nbPageFin > 3 ? data.toRead.pageArray[nbPageStart + 3].content : "",
+        nbPageFin > 4 ? data.toRead.pageArray[nbPageStart + 4].content : ""
       );
+      console.log(nowBlock);
 
       previousHash = hash;
     }
-    stmt.finalize();
 
-    db.each("SELECT * FROM blocks limit 4", function(err, row) {
-      console.log(row);
-    });
+    mineBlock(1);
+
+    stmt.finalize();
+    // db.each("SELECT * FROM blocks limit 4", function(err, row) {
+    //   console.log(row);
+    // });
   });
 });
 
