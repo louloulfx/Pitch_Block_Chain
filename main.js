989f898fc2bac3c35b255032094f58af77f8b0e7
Elouan <louloulfx1@hotmail.fr>
2020-01-28 14:34:22 +0100

    new branch

--- main.js
+++ main.js
@@ -2,55 +2,55 @@ const crypto = require("crypto");
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./text.sql");
 const fs = require("fs");
+const express = require("express");
+// class Block {
+//   constructor(index, data, previousHash) {
+//     this.index = index;
+//     this.timestamp = Math.floor(Date.now() / 1000);
+//     this.data = data;
+//     this.previousHash = previousHash;
+//     this.hash = this.getHash();
+//     console.log(this.hash);
+//   }
 
-class Block {
-  constructor(index, data, previousHash) {
-    this.index = index;
-    this.timestamp = Math.floor(Date.now() / 1000);
-    this.data = data;
-    this.previousHash = previousHash;
-    this.hash = this.getHash();
-    console.log(this.hash);
-  }
+//   getHash() {
+//     var encript =
+//       JSON.stringify(this.data) +
+//       this.previousHash +
+//       this.index +
+//       this.timestamp;
+//     var hash = crypto
+//       .createHmac("sha256", "bVwDy-R4CyM-VdPnW-pV2Fc-88F9M")
+//       .update(encript)
+//       .digest("hex");
+//     return hash;
+//   }
+// }
 
-  getHash() {
-    var encript =
-      JSON.stringify(this.data) +
-      this.previousHash +
-      this.index +
-      this.timestamp;
-    var hash = crypto
-      .createHmac("sha256", "bVwDy-R4CyM-VdPnW-pV2Fc-88F9M")
-      .update(encript)
-      .digest("hex");
-    return hash;
-  }
-}
+// class BlockChain {
+//   constructor() {
+//     this.chain = [];
+//   }
 
-class BlockChain {
-  constructor() {
-    this.chain = [];
-  }
+//   addBlock(data) {
+//     let index = this.chain.length;
+//     let previousHash =
+//       this.chain.length !== 0 ? this.chain[this.chain.length - 1].hash : 0;
+//     let block = new Block(index, data, previousHash);
+//     this.chain.push(block);
+//   }
 
-  addBlock(data) {
-    let index = this.chain.length;
-    let previousHash =
-      this.chain.length !== 0 ? this.chain[this.chain.length - 1].hash : 0;
-    let block = new Block(index, data, previousHash);
-    this.chain.push(block);
-  }
+//   chainIsValid() {
+//     for (var i = 0; i < this.chain.length; i++) {
+//       if (this.chain[i].hash !== this.chain[i].getHash()) return false;
+//       if (i > 0 && this.chain[i].previousHash !== this.chain[i - 1].hash)
+//         return false;
+//     }
+//     return true;
+//   }
+// }
 
-  chainIsValid() {
-    for (var i = 0; i < this.chain.length; i++) {
-      if (this.chain[i].hash !== this.chain[i].getHash()) return false;
-      if (i > 0 && this.chain[i].previousHash !== this.chain[i - 1].hash)
-        return false;
-    }
-    return true;
-  }
-}
-
-const BChain = new BlockChain();
+// const BChain = new BlockChain();
 
 let toRead = new Promise((resolve, reject) => {
   fs.readFile("./allPages.json", (err, data) => {
