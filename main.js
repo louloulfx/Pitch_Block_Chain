c59f2aa82a491574a7461fe3639a1fb83edca137
Kobweb <yugoplixgamer@gmail.com>
2020-01-28 09:17:27 +0100

    Python PDF to json and DB connector finished

--- main.js
+++ main.js
@@ -1,28 +1,53 @@
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./text.sql");
-
-db.serialize(function() {
-  db.run(`CREATE TABLE IF NOT EXISTS blocks (
-      idBlock INTEGER PRIMARY KEY AUTOINCREMENT,
-      previousHash TEXT,
-      nbPageStart INTEGER,
-      nbPageFinish INTEGER,
-      firstregistration TEXT,
-      secondRegistration TEXT,
-      thirdRegistration TEXT,
-      fourthRegistration TEXT,
-      fifthRegistration TEXT,
-      timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`);
-
-  let stmt = db.prepare(
-    "INSERT INTO blocks (previousHash, nbPageStart, nbPageFinish, firstRegistration, secondRegistration, thirdRegistration, fourthRegistration, fifthRegistration) VALUES (?,?,?,?,?,?,?,?)"
-  );
-  stmt.run("anyHash", 0, 1, "test1", "test2", "test3", "test4", "test5");
-  stmt.finalize();
-
-  db.each("SELECT * FROM blocks", function(err, row) {
-    console.log(row);
+const fs = require("fs");
+let toRead = new Promise((resolve, reject) => {
+  fs.readFile("./allPages.json", (err, data) => {
+    if (err) throw err;
+    resolve({ toRead: JSON.parse(data) });
   });
 });
 
-db.close();
+toRead.then(data => {
+  console.log(data.toRead.pageArray.length);
+  console.log("here");
+  db.serialize(function() {
+    db.run(`CREATE TABLE IF NOT EXISTS blocks (
+        idBlock INTEGER PRIMARY KEY AUTOINCREMENT,
+        previousHash TEXT,
+        nbPageStart INTEGER,
+        nbPageFinish INTEGER,
+        firstregistration TEXT,
+        secondRegistration TEXT,
+        thirdRegistration TEXT,
+        fourthRegistration TEXT,
+        fifthRegistration TEXT,
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`);
+
+    let stmt = db.prepare(
+      "INSERT INTO blocks (previousHash, nbPageStart, nbPageFinish, firstRegistration, secondRegistration, thirdRegistration, fourthRegistration, fifthRegistration) VALUES (?,?,?,?,?,?,?,?)"
+    );
+    for (let i = 0; i < data.toRead.pageArray.length; i += 5) {
+      iterationLeft = data.toRead.pageArray.length - i;
+      stmt.run(
+        "anyHash",
+        data.toRead.pageArray[i].nbPage,
+        iterationLeft > 5
+          ? data.toRead.pageArray[i + 4].nbPage
+          : data.toRead.pageArray.length,
+        data.toRead.pageArray[i].content,
+        iterationLeft > 1 ? data.toRead.pageArray[i + 1].content : "",
+        iterationLeft > 2 ? data.toRead.pageArray[i + 2].content : "",
+        iterationLeft > 3 ? data.toRead.pageArray[i + 3].content : "",
+        iterationLeft > 4 ? data.toRead.pageArray[i + 4].content : ""
+      );
+    }
+    stmt.finalize();
+
+    db.each("SELECT * FROM blocks", function(err, row) {
+      console.log(row);
+    });
+  });
+
+  db.close();
+});
